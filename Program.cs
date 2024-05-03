fc6a9b906b445e108e4a4514450023bb6d685f99
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:29:59 -0300

    ATIVIDADE CONCLUIDA

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double numerador, denominador, quociente;
+Console.WriteLine("--- ATIVIDADE DE DIVISÃO ---");
+
+Console.Write("Digite o Numerador....:");
+numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o Denominador....:");
+denominador = Convert.ToDouble(Console.ReadLine());
+
+quociente = numerador / denominador;
+
+
+
+if (denominador == 0)
+{
+    Console.WriteLine("Não é possível dividir por zero.");
+   return;
+}
+
+ Console.WriteLine($"O resultado é {quociente} ");
+
